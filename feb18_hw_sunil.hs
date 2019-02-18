884e6956983a64ce310187d7134a9fb884da9089
Sunil KS <kslvsunil@gmail.com>
2019-02-18 05:52:15 +0100

    Add files via upload
    HW for Feb 18 - Sunil

---
+++ feb18_hw_sunil.hs
@@ -0,0 +1,50 @@
+square :: Int -> Int
+square m = m*m
+
+max2 :: Int -> Int -> Int
+max2 n1 n2
+  | n1 > n2 = n1
+  | n2 > n1 = n2
+  | otherwise = n1
+
+-- HW 8 Grade
+grade :: Int -> String
+grade g
+  | g == 5 = "Excellent"
+  | g == 4 = "Good"
+  | g == 3 = "Fair"
+  | g == 2 = "Sufficient"
+  | g == 1 = "Fail"
+  | otherwise = "not a grade!"
+
+-- HW 9 Pythagorean Triple
+pythagorean_triple :: Int -> Int -> Int -> Bool
+pythagorean_triple n1 n2 n3
+  | square n1 + square n2 == square n3 = True
+  | otherwise = False
+
+-- HW 10 Maximum of 3 integers
+max3 :: Int -> Int -> Int -> Int
+max3 n1 n2 n3
+  | n3 > max2 n1 n2 = n3
+  | otherwise = max2 n1 n2
+
+-- Own HW 1 Factorial
+factorial :: Int -> Int
+factorial n
+  | n == 0 = 1
+  | otherwise = n * factorial (n-1)
+
+-- Own HW 2 Decimal to Binary
+binary :: Int -> String
+binary n
+  | n == 0 = "0"
+  | n == 1 = "1"
+  | n `rem` 2 == 0 = binary (n `div` 2) ++ "0"
+  | otherwise = binary (n `div` 2) ++ "1"
+
+-- Own HW 3 Check if Leap year
+leap_year :: Int -> Bool
+leap_year y
+  | y `rem` 4 == 0 = True
+  | otherwise = False
