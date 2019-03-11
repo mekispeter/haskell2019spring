4033c296293e5e6d0e8cf2ae68e60d3cb81a448c
mekispeter <mekis.peter@skool.org.hu>
2019-03-11 18:33:57 +0100

    cn

--- haskell_march11.hs
+++ haskell_march11.hs
@@ -23,8 +23,16 @@ fiblist' 0 = [0]
 fiblist' 1 = [1,0]
 fiblist' n = (head oldlist
             + head (tail oldlist))
-            : take 2 oldlist where
+            : [head oldlist] where
               oldlist = fiblist' (n-1)
 
 fibfromlist' :: Integer -> Integer
 fibfromlist' n = head (fiblist' n)
+
+fibonacciSmart :: Integer -> Integer
+fibonacciSmart n = fibAux n 0 1 where
+  fibAux :: Integer -> Integer ->
+            Integer -> Integer
+  fibAux 0 x y = y
+  fibAux n x y = fibAux (n-1) y (x+y)
+  
