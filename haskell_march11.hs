0476e4794c08c699d1d329014eef7b51e27d77ca
mekispeter <mekis.peter@skool.org.hu>
2019-03-11 18:16:31 +0100

    session code updated

--- haskell_march11.hs
+++ haskell_march11.hs
@@ -2,3 +2,15 @@
   Functional Programming for Logicians
   March 11
 -}
+fibdummy :: Integer -> Integer
+fibdummy 0 = 0
+fibdummy 1 = 1
+fibdummy n = fibdummy (n-1) + fibdummy (n-2)
+
+fiblist :: Integer -> [Integer]
+fiblist 0 = [0]
+fiblist 1 = [1,0]
+fiblist n = (head oldlist
+            + head (tail oldlist))
+            : oldlist where
+              oldlist = fiblist (n-1)
