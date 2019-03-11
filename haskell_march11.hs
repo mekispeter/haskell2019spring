9dc02f9427821ff4b69354ac2551d9c489621c3a
mekispeter <mekis.peter@skool.org.hu>
2019-03-11 19:08:53 +0100

    ssss

--- haskell_march11.hs
+++ haskell_march11.hs
@@ -42,3 +42,22 @@ fibSmart2 n = snd (fibAux2 n) where
   fibAux2 1 = (0,1)
   fibAux2 n = (second, first + second) where
     (first,second) = fibAux2 (n-1)
+
+type IntRel = Integer -> Integer -> Bool
+type IntProp = Integer -> Bool
+
+type CharRel = Char -> Char -> Bool
+type CharProp = Char -> Bool
+
+type Relation a = a -> a -> Bool
+type Property a = a -> Bool
+
+flipArg :: Relation a -> Relation a
+(flipArg r) x y = r y x
+
+isPal :: (Eq a) => Property [a]
+isPal []              = True
+isPal (c:[])          = True
+isPal s
+  | head s == last s  = isPal (init (tail s))
+  | otherwise         = False
